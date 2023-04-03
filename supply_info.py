12f297337fe65a386c02470f4b8b194467ce9c13
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-04-03 14:45:52 -0700

    create table of supplies

---
+++ supply_info.py
@@ -0,0 +1,8 @@
+etoh_waste_supplies = {'18_gal_container_empty': 0,
+                       '18_gal_container_full': 0,
+                       '5_gal_carboy_empty': 0,
+                       '5_gal_carboy_full': 0,
+                       'biohaz_box_empty': 0,
+                       'biohaz_box_full': 0
+                       }
+
