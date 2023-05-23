a0e10f4d3452cb8dddc66df26319f75a28dce272
Phoenix Tagal <116997916+PhoenixTagal@users.noreply.github.com>
2023-05-23 10:07:08 -0700

    Update sequencing_supply_info.py

--- sequencing_supply_info.py
+++ sequencing_supply_info.py
@@ -9,8 +9,9 @@ sequencing_waste_supplies = {'15_gal_poly_empty_$': 66,
                              'nova_seq_waste_volume_per_run_ml': 18800, #from s4 300 cycle dual side
                              'sequencing_drum_volume_in_ml': 56781.2,
                              'samples_analyzed_per_year': 0,
-                             'samples_analyzed_per_nextseq_run': 0, #8-96 samples per run,  Runs performed in Dx Mode are processes regulated by US and EU regulatory bodies
-                             'samples_analyzed_per_novaseq_run': 0 #can be samples per run or reads per run?
+                             'clonoseq_samples_analyzed_per_nextseq_run': 24, #provided by illumina rep
+                             'clonoseq_samples_analyzed_per_novaseq_run': 576, #provided by illumina rep
+                             'total_clonoseq_samples_analyzed_in_2022': 55480 #sum of ID and MRD year end totals
                             }
 
 
@@ -18,14 +19,12 @@ def sequencing_life_cycle_cost():
     total_cost = sequencing_waste_supplies['15_gal_poly_empty_$'] + sequencing_waste_supplies['15_gal_poly_disposal_$']
     return total_cost
 
-
 sequencing_waste_supplies['sequencing_drum_life_cycle_cost'] = sequencing_life_cycle_cost()
 
 
 def next_seq_runs_per_sequencing_drum():
     runs_per_drum = sequencing_waste_supplies['sequencing_drum_volume_in_ml'] / sequencing_waste_supplies['next_seq_waste_volume_per_run_ml']
     return round(runs_per_drum)  #rounding runs per drum is necessary to make sense
-
 
 sequencing_waste_supplies['next_seq_runs_per_sequencing_drum'] = next_seq_runs_per_sequencing_drum()
 
@@ -39,13 +38,28 @@ def nova_seq_runs_per_sequencing_drum():
     nova_runs_per_drum = sequencing_waste_supplies['sequencing_drum_volume_in_ml'] / sequencing_waste_supplies['nova_seq_waste_volume_per_run_ml']
     return round(nova_runs_per_drum)
 
-
 sequencing_waste_supplies['nova_seq_runs_per_sequencing_drum'] = nova_seq_runs_per_sequencing_drum()
 
 
 def cost_per_nova_seq_run():
     nova_run_cost = sequencing_waste_supplies['sequencing_drum_life_cycle_cost'] / sequencing_waste_supplies['nova_seq_runs_per_sequencing_drum']
     return round(nova_run_cost)
+
+
+"""The following functions are used to calculate disposal cost based on number of colonoseq samples analyzed in 2022"""
+
+
+def next_seq_annual_cost():
+    total_next_seq_runs = sequencing_waste_supplies['total_clonoseq_samples_analyzed_in_2022'] / sequencing_waste_supplies['clonoseq_samples_analyzed_per_nextseq_run']
+    next_seq_annual_disposal_cost = total_next_seq_runs * cost_per_next_seq_run()
+    return round(next_seq_annual_disposal_cost)
+
+
+
+def nova_seq_annual_cost():
+    total_nova_seq_runs = sequencing_waste_supplies['total_clonoseq_samples_analyzed_in_2022'] / sequencing_waste_supplies['clonoseq_samples_analyzed_per_novaseq_run']
+    nova_seq_annual_disposal_cost = total_nova_seq_runs * cost_per_nova_seq_run()
+    return round(nova_seq_annual_disposal_cost)
 
 
 print(f'Total cost of sequencing drum (supply cost + disposal cost): ${sequencing_life_cycle_cost()}')
@@ -55,4 +69,9 @@ print(f'Disposal cost of one Nova Seq run: ${cost_per_next_seq_run()}')
 print('')
 print(f'Nova Seq runs per sequencing waste drum: {nova_seq_runs_per_sequencing_drum()}')
 print(f'Disposal cost of one Next Seq run: ${cost_per_nova_seq_run()}')
+print('')
+print('')
+print(f'Next Seq Annual Disposal Spend: ${next_seq_annual_cost()}')
+print(f'Nova Seq Annual Disposal Spend: ${nova_seq_annual_cost()}')
+print(f'Annual Savings gained by using Nova Seq: ${next_seq_annual_cost() - nova_seq_annual_cost()}')
 
